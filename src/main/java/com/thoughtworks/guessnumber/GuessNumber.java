e471e8dcb1601de6a96f3f1c07d1491697a6987c
chenze.li <chenze.li@thoughtworks.com>
2020-01-27 20:18:27 +0800

    [Chenze]#GuessNumber  finish the guess part to solve other situation

--- src/main/java/com/thoughtworks/guessnumber/GuessNumber.java
+++ src/main/java/com/thoughtworks/guessnumber/GuessNumber.java
@@ -4,12 +4,24 @@ import java.util.List;
 
 public class GuessNumber {
 
-  public Result guess(List<Integer> input, List<Integer> answer) {
-    if (answer.retainAll(input)) {
-      return Result.builder()
-          .outPut("Wrong Input, input again")
-          .instruction("Wrong Input, input again").build();
+  public String guess(List<Integer> input, List<Integer> answer) {
+    int positionRight = 0;
+    int valueRight = 0;
+    for (int i = 0; i < input.size(); i++) {
+      for (int j = 0; j < answer.size(); j++) {
+        if (input.get(i).equals(answer.get(j))) {
+          if (i == j) {
+            positionRight++;
+          } else {
+            valueRight++;
+          }
+        }
+      }
     }
-      return null;
+    return getOutPut(positionRight, valueRight);
+  }
+
+  private String getOutPut(int positionRight, int valueRight) {
+    return positionRight + "A" + valueRight + "B";
   }
 }
--- src/test/java/com/thoughtworks/guessnumber/GuessNumberTest.groovy
+++ src/test/java/com/thoughtworks/guessnumber/GuessNumberTest.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
 class GuessNumberTest extends Specification {
   def guessNumber = new GuessNumber()
 
-  def 'it can return wrong input when guess wrong'() {
+  def 'it can return all wrong'() {
     given:
       def inputList = [5, 6, 7, 8]
       def answerList = [1, 2, 3, 4]
@@ -14,8 +14,18 @@ class GuessNumberTest extends Specification {
       def result = guessNumber.guess(inputList, answerList)
 
     then:
-      result.outPut == "Wrong Input, input again"
-      result.instruction == "Wrong Input, input again"
+      result == "0A0B"
   }
 
+  def 'it can return 1 correct input '() {
+    given:
+      def inputList = [1, 5, 6, 7]
+      def answerList = [1, 2, 3, 4]
+
+    when:
+      def result = guessNumber.guess(inputList, answerList)
+
+    then:
+      result == "1A0B"
+  }
 }
